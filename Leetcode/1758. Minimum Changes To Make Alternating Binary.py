fc43c7dfad01a4fa230aded20a1f65971493b6fc
ANUBHAV2310964 <2k23.aids2310964@gmail.com>
2026-03-05 18:08:39 +0530

    Implement solution for minimum changes to binary string

---
+++ Leetcode/1758. Minimum Changes To Make Alternating Binary.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, s: str) -> int:
+        count = 0
+
+        for i, c in enumerate(s):
+            if c != "01"[i % 2]:
+                count += 1
+
+        return min(count, len(s) - count)
